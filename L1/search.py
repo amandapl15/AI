a0913e9000d23bb35845cbe2ee09f4ed69bfb91c
Amanda Pintado <39129859+amandapl15@users.noreply.github.com>
2022-05-01 20:43:27 +0200

    Update search.py

--- L1/search.py
+++ L1/search.py
@@ -11,7 +11,6 @@
 # Student side autograding was added by Brad Miller, Nick Hay, and
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
-#Antonio Pintado u172771 y Amanda Pintado u137702
 
 """
 In search.py, you will implement generic search algorithms which are called by
